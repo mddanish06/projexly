e02abb257a997c2474246e071044aecfdd549250
Danish <danishsharique31@gmail.com>
2025-04-16 01:04:47 +0530

    updated

--- backend/src/main/java/com/PMS/model/Issue.java
+++ backend/src/main/java/com/PMS/model/Issue.java
@@ -19,7 +19,7 @@ import lombok.Data;
 @Data
 public class Issue {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String title;
