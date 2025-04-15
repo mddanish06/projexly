3710602fe2314a893d3b595ad8ac004406107cc0
Danish <danishsharique31@gmail.com>
2025-04-15 21:13:39 +0530

    updated

--- backend/src/main/java/com/PMS/ProjectManagementSystemApplication.java
+++ backend/src/main/java/com/PMS/ProjectManagementSystemApplication.java
@@ -7,6 +7,8 @@ public class ProjectManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectManagementSystemApplication.class, args);
+		System.out.println("ENV variable: " + System.getenv("ENV"));
+
 	}
 
 }
