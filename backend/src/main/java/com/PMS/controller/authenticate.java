2290555d8d8f9e39fbc60f23ca3fc827e60d78bf
Danish <danishsharique31@gmail.com>
2025-04-29 14:37:36 +0530

    added home url

--- backend/src/main/java/com/PMS/controller/authenticate.java
+++ backend/src/main/java/com/PMS/controller/authenticate.java
@@ -1,5 +1,13 @@
 package com.PMS.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class authenticate {
 
+    @GetMapping("/")
+    public String home() {
+        return "Backend is running successfully!";
+    }
 }
